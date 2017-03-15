99f37f54d3901e66eab06540f6c622fa2d8ea62a
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2017-03-15 09:07:48 -0300

    Adicionados Comentarios no Codigo

--- comando.js
+++ comando.js
@@ -1,15 +1,27 @@
 "use strict"
 
+//VAMOS USAR A READLINE PARA LER O VALOR DIGITADO
 const readline = require('readline')
+
+//COLORS E TABLE SÃO APENAS PARA A FORMATAÇÃO DO TEXTO
 const colors = require('colors')
 const Table = require('cli-table')
 
+//CRIANDO A INTERFACE DE READLINE
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
+//COMANDO inicio
 let inicio = function(){
+  
+  /**
+   * CASO NÃO QUEIRA USAR AS DEPENDÊNCIAS PARA A FORMATAÇÃO
+   * DO TEXTO. UTILIZE AS LINHAS COMENTADAS ABAIXO NO LUGAR
+   * DA CRIAÇÃO DE TABLE.
+   */
+  
   /*
   console.log('\n')
   console.log('+------------------------+')
@@ -17,21 +29,39 @@ let inicio = function(){
   console.log('+------------------------+')
   console.log('\n')
   */
+
+  //CRIAÇÃO E FORMATAÇÃO DA TABELA QUE SERÁ EXIBIDA
+  //CONSULTE A DOCUMENTAÇÃO DO cli-table NO SITE DO NPM
   let table = new Table({
       head: ['          INICIANDO PROGRAMA'],
       colWidths: [40],
       style: {
         head: ['black','bold']//,
-        //border: ['green']
       }
   })
+  
+  //EXIBE A TABELA
   console.log(table.toString())
 }
 
+//COMANDO copy
 let copy = function(){
-    //console.log('\n')
-    //console.log('(c)2017 - Estúdio Digital Bocca'.green)
-    //console.log('\n')
+    
+    /**
+     * CASO NÃO QUEIRA USAR AS DEPENDÊNCIAS PARA A FORMATAÇÃO
+     * DO TEXTO. UTILIZE AS LINHAS COMENTADAS ABAIXO NO LUGAR
+     * DA CRIAÇÃO DE TABLE.
+     */  
+    
+    /*
+    console.log('\n')
+    console.log('(c)2017 - Estúdio Digital Bocca'.green)
+    console.log('\n')
+    */
+    
+
+    //CRIAÇÃO E FORMATAÇÃO DA TABELA QUE SERÁ EXIBIDA
+    //CONSULTE A DOCUMENTAÇÃO DO cli-table NO SITE DO NPM
     let table = new Table({
           head: ['   (c)2017 - Estúdio Digital Bocca'],
           colWidths: [40],
@@ -40,13 +70,17 @@ let copy = function(){
             border: ['green']
           }
     })
+
+    //EXIBE A TABELA
     console.log(table.toString())
 }
 
+//ENCERRA A EXECUÇÃO
 let encerrar = function(){
   rl.close()
 }
 
+//FUNÇÃO QUE FICA AGUARDANDO O COMANDO
 let comandoPrompt = function(){
   
   rl.question('COMANDO: ', (comando) => {
@@ -80,4 +114,5 @@ let comandoPrompt = function(){
   })
 }
 
+//INICIA O PROGRAMA.
 comandoPrompt()
